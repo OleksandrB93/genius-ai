822cfbd3060ab7dfc104561231d6aa88d5109116
OleksandrB93 <barabashsan4ik@gmail.com>
2023-07-21 00:02:54 +0300

    created sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 const DashboardPage = () => {
   return (
-    <p className="m-4">
+    <p className="">
       DashboardPage (Protected)
-      <UserButton afterSignOutUrl="/" />
     </p>
   );
 };
